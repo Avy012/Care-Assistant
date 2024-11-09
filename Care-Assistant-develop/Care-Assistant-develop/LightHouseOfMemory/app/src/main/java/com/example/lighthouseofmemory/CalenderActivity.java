da0b2fb03a526bf093ad42649c7cdd66de93b2f1
Avy012 <kist0907@gmail.com>
2024-11-10 00:07:16 +0900

    logout dialog added

--- Care-Assistant-develop/Care-Assistant-develop/LightHouseOfMemory/app/src/main/java/com/example/lighthouseofmemory/CalenderActivity.java
+++ Care-Assistant-develop/Care-Assistant-develop/LightHouseOfMemory/app/src/main/java/com/example/lighthouseofmemory/CalenderActivity.java
@@ -1,14 +1,39 @@
 package com.example.lighthouseofmemory;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class CalenderActivity extends AppCompatActivity {
+
+    private Button medicineB;
+    private Button waterB;
+    BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
+
+        medicineB = findViewById(R.id.medicine_button);
+        waterB = findViewById(R.id.water_button);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.navigation_gps) {
+                    startActivity(new Intent(CalenderActivity.this, GpsActivity.class));
+                    return true;
+                }
+                return false;
+            }
+
+        });
     }
 }
